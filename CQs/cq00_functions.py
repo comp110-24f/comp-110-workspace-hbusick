ff4d34340f03c00fbda921886b9ac616c70155c2
Hunter Busick <hbusick@unc.edu>
2024-08-27 12:19:30 -0400

    completed cq00. Saving functions lesson and ex00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,12 @@
+"""Functions Challenege Question"""
+
+__author__ = "730563704"
+
+
+def mimic(message: str) -> str:
+    "returns a message"
+    return message
+
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message?")))
---
+++ Lessons/functions.py
@@ -0,0 +1,9 @@
+"""Practice with functions"""
+
+
+def sum(num1: int, num2: int) -> int:
+    """Return num1 + num2"""
+    return num1 + num2
+
+
+print(sum(num1=5, num2=8))  # <- 5 and 8 are the arguments of this function call
