4d2161adf84d6e2f68aa65f19eaf7b23332fd689
Hunter Busick <hbusick@unc.edu>
2024-08-27 12:21:40 -0400

    Deleted duplicate of ex00

--- exercises/ex00_hello_world.py
+++
@@ -1,8 +0,0 @@
-"""My first exercise in COMP110!"""
-
-__author__ = "730563704"
-
-
-def greet(name: str) -> str:
-    """A welcoming first function definition."""
-    return "Hello, " + name + "!"
