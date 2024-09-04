5f57dc7869aa5dfd8190af97df6b27c742baedb4
Hunter Busick <hbusick@unc.edu>
2024-09-04 13:35:33 -0400

    Completed ex01

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -4,41 +4,39 @@ __author__: str = "730563704"
 
 
 def main_planner(guests: int) -> None:
-    "runs event plans"
+    "runs event plans"  # Values must all ultimately come from the guests input
     print("A Cozy Tea Party For", guests, "People!")
-    print("Tea Bags: ", tea_bags(people=guests))
-    print("Treats: ", treats(people=guests))
-    print("Cost: $", cost(tea_bags(people=guests), treat_count=treats(people=guests)))
-    # The only input for this function is guests...
-    # so tea and treat count must = guests?
-    # Work backwards from initial tea_bags and treats functions to get a value that you
-    # then use in the cost function
+    print("Tea Bags:", tea_bags(people=guests))
+    print(
+        "Treats:", treats(people=guests)
+    )  # people = guests these are basically synonomous throughout
+    print(
+        "Cost: $"
+        + str(
+            cost(tea_bags(people=guests), treat_count=treats(people=guests))
+        )  # nest the tea bag and treat functions within the cost function
+    )  # Convert cost to a string to solve the empty space after "$"
 
 
 def tea_bags(people: int) -> int:
     "number of tea bags for # attending"
-    tea_bags = int(people * 2)
-    return tea_bags
+    return int(people * 2)
 
 
 def treats(people: int) -> int:
     "treats for given # of guests"
-    teas = tea_bags(people)
-    # call the tea bag function to get number of teas
-    # define as its own new variable
-    treats = int(teas * 1.5)
-    return treats
+    return int(tea_bags(people=people) * 1.5)
+
+
+# use people to get total tea bags first
 
 
 def cost(tea_count: int, treat_count: int) -> float:
     "takes a given amount of teas and treats to calculate cost"
-    tea_cost = tea_count * 0.5
-    treat_cost = treat_count * 0.75
-    # define new variables for storing calculations
-    cost = tea_cost + treat_cost
-    # define total cost
-    return cost
+    return (tea_count * 0.5) + (treat_count * 0.75)
 
+
+# how we get these values makes more sense later in the main planner
 
 if __name__ == "__main__":
     main_planner(guests=int(input("How many guests are attending your tea party? ")))
