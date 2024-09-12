1d0a6f42a8358cb668dd668327766e0e9b05dedf
Hunter Busick <hbusick@unc.edu>
2024-09-12 08:43:11 -0400

    PSYC work

--- Lessons/Booleans&Conditionals.py
+++ Lessons/Booleans&Conditionals.py
@@ -1,1 +1,11 @@
 """Booleans and Conditionals Lecture"""
+
+
+def get_weather_report() -> None:
+    weather = str(input("What is the weather?"))
+    if weather == "rainy" or weather == "cold":
+        print("Bring a jacket")
+    elif weather == "hot":
+        print("Keep it cool")
+    else:
+        print("Weather not recognized")
